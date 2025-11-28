92365eb74a07883bfd7a1ee8f4db570c1aed8617
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-11-28 15:06:34 +0100

    numpy vs python

--- main.py
+++ main.py
@@ -4,8 +4,30 @@ import random
 
 
 
-def _fill_list():
+def _fill_list(iterations=10**7):
     data = []
-    for _ in range(1000):
-        data.append(random.randint(1, 2**8))
+    for _ in range(round(iterations)):
+        data.append(random.randint(1, 10**4))
     return data
+
+
+if __name__ == "__main__":
+    arr1 = _fill_list()
+
+    # python
+    start = time.time()
+    result1 = [x + x for x in arr1]
+    end = time.time()
+    print(len(result1))
+    print(f"Standard python: {end-start}") 
+
+    # Numpy
+    np_arr1 = np.array(arr1)
+
+
+    start = time.time()
+
+    result2 = np_arr1 + np_arr1
+    end = time.time()
+    print(f"Numpy time: {end-start}") 
+    print(result1 == result2)
